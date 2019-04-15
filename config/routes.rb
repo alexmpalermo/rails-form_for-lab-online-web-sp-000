438f64dfdcfc403ccb945b2f9689f137195e1e33
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-15 17:58:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
- resources :posts, only: %i[index show new create edit update]
-
+ #resources :posts, only: %i[index show new create edit update]
+  resources :students
+  resources :school_classes
 end
